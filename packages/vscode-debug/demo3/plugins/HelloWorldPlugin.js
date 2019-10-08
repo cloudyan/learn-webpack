f8e009ef06dcc09c6fcafa1d20c1c71b03230fed
jack <1395093509@qq.com>
2019-10-08 17:59:43 +0800

    test hooks

--- packages/vscode-debug/demo3/plugins/HelloWorldPlugin.js
+++ packages/vscode-debug/demo3/plugins/HelloWorldPlugin.js
@@ -1,4 +1,6 @@
 // https://webpack.js.org/contribute/writing-a-plugin/
+// https://webpack.docschina.org/contribute/writing-a-plugin/
+// https://webpack.docschina.org/api/plugins/
 
 class HelloWorldPlugin {
   constructor(options) {
@@ -9,11 +11,51 @@ class HelloWorldPlugin {
     console.log(compiler);
     debugger;
 
+    compiler.hooks.entryOption.tap('HelloWorldPlugin2', xx => {
+      console.log(xx);
+      console.log('entryOption');
+    });
+
+    // tap(触及) 到 compilation hook，而在 callback 回调时，会将 compilation 对象作为参数，
+    compiler.hooks.compilation.tap('HelloCompilationPlugin', compilation => {
+      // 现在，通过 compilation 对象，我们可以 tap(触及) 到各种可用的 hooks 了
+      compilation.hooks.optimize.tap('HelloCompilationPlugin', () => {
+        console.log('正在优化资源。');
+      });
+    });
+
     compiler.hooks.done.tap('HelloWorldPlugin', stats => {
+      // 在 hook 被触及时，会将 stats 作为参数传入
       // stats is passed as argument when done hook is tapped.
       console.log('Hello World!');
       console.log(this.options);
     });
+
+    // compiler.hooks.compile.tap('MyPlugin', params => {
+    //   console.log('以同步方式触及 compile 钩子。');
+    // });
+
+    // compiler.hooks.run.tapAsync(
+    //   'MyPlugin',
+    //   (source, target, routesList, callback) => {
+    //     console.log('以异步方式触及 run 钩子。');
+    //     typeof callback === 'function' && callback();
+    //   }
+    // );
+
+    // compiler.hooks.run.tapPromise('MyPlugin', (source, target, routesList) => {
+    //   return new Promise(resolve => setTimeout(resolve, 1000)).then(() => {
+    //     console.log('以具有延迟的异步方式触及 run 钩子。');
+    //   });
+    // });
+
+    // compiler.hooks.run.tapPromise(
+    //   'MyPlugin',
+    //   async (source, target, routesList) => {
+    //     await new Promise(resolve => setTimeout(resolve, 1000));
+    //     console.log('以具有延迟的异步方式触及 run 钩子。');
+    //   }
+    // );
   }
 }
 
